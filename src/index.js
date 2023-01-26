b741a4be67f81bee3ea5d414b0fa2346a9de84dc
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-01-26 16:32:09 +0300

    Fetching data on demand

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const init = () => {
         const input = document.querySelector('input#searchByID');
 
         console.log(input.value);
-
         fetch(`http://localhost:3000/movies/${input.value}`)
         .then(resp => resp.json())
         .then(data => {
